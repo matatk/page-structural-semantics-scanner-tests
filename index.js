681df7820bb331316291b90312038982e79b6846
Matthew Tylee Atkinson <matatk@gmail.com>
2019-04-14 15:28:29 +0100

    feat: Export test iterator function
    Also DRY the making of the custom runner functions.

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const path = require('path')
 const tap = require('tap')
 const runner = require('./lib/runner')
 const converters = require('./lib/converters')
+const iterator = require('./lib/iterator')
 
 const fixtures = path.join(__dirname, 'fixtures')
 const expectations = path.join(__dirname, 'expectations')
 
-const landmarksRunner = function(scanner) {
-	runner(tap, fixtures, expectations, converters.landmarks, scanner)
-}
-
-const a11yOutlineRunner = function(scanner) {
-	runner(tap, fixtures, expectations, converters.a11yOutline, scanner)
+function makeRunner(converter) {
+	return function(scanner) {
+		runner(tap, fixtures, expectations, converter, scanner)
+	}
 }
 
 module.exports = {
-	'runners': {
-		'landmarks': landmarksRunner,
-		'a11yOutline': a11yOutlineRunner
+	'runner': function(converter, scanner) {
+		runner(tap, fixtures, expectations, converter, scanner)
 	},
-	'converters': converters
+	'runners': {
+		'landmarks': makeRunner(converters.landmarks),
+		'a11yOutline': makeRunner(converters.a11yOutline)
+	},
+	'converters': converters,
+	'iterator': iterator
 }
