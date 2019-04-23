16dc9be433b89deab52f1934812e168f65858ce1
Matthew Tylee Atkinson <matatk@gmail.com>
2019-04-23 09:02:45 +0100

    (re-)add check function - but separately this time

--- test/check.test.js
+++ test/check.test.js
@@ -3,50 +3,45 @@ const tap = require('tap')
 const check = require('../lib/check')
 
 const exampleResult = [
-	{
-		role: 'banner'
-	},
-	{
-		role: 'main'
-	},
-	{
-		role: 'contentinfo'
-	}
+	{ role: 'banner' },
+	{ role: 'main' },
+	{ role: 'contentinfo' }
 ]
 
-const expectedResultEqual = [
-	{
-		role: 'banner'
-	},
-	{
-		role: 'main'
-	},
-	{
-		role: 'contentinfo'
-	}
-]
+tap.test('When there is a match', t => {
+	const expectedResultEqual = [
+		{ role: 'banner' },
+		{ role: 'main' },
+		{ role: 'contentinfo' }
+	]
 
-const expectedResultDiffernt = [
-	{
-		role: 'banner'
-	},
-	{
-		role: 'navigation'
-	},
-	{
-		role: 'main'
-	},
-	{
-		role: 'contentinfo'
-	}
-]
+	t.strictSame(
+		check(exampleResult, expectedResultEqual),
+		{ matches: true },
+		'Returns true (and no patch) when the result matches the expectation')
 
-tap.equal(
-	check(exampleResult, expectedResultEqual),
-	true,
-	'Returns true when the result matches the expectation')
+	t.end()
+})
 
-tap.equal(
-	check(exampleResult, expectedResultDiffernt),
-	false,
-	"Returns false when the result doesn't match the expectation")
+tap.test("When there isn't a match", t => {
+	const expectedResultDiffernt = [
+		{ role: 'banner' },
+		{ role: 'navigation' },
+		{ role: 'main' },
+		{ role: 'contentinfo' }
+	]
+
+	const result = check(exampleResult, expectedResultDiffernt)
+
+	t.equal(
+		result.matches,
+		false,
+		"Returns false when the result doesn't match the expectation")
+
+	t.equal(
+		result.patch,
+		'',
+		"Returns a patch when the result doesn't match the expectation")
+
+	t.end()
+})
