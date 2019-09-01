1e8ab3632da9a985043ecf2760a3d40e5e90c659
Matthew Tylee Atkinson <matatk@gmail.com>
2019-09-02 00:00:28 +0100

    Simplify index.js to fix ESLint errors

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict'
-const path = require('path')
-
-const fixtures = path.join(__dirname, 'fixtures')
-const expectations = path.join(__dirname, 'expectations')
-
 module.exports = {
 	'nowt': null
 }
